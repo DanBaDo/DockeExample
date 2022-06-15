46e53c3027302f2147ffa8ac0981f72989f7d436
DanBaDo <byte4life@pm.me>
2022-06-15 11:59:11 +0200

    Changes / URL folder

--- backend/index.mjs
+++ backend/index.mjs
@@ -4,7 +4,7 @@ const app = express()
 
 const food = ["tortilla", "sopa", "ensalada", "aceitunas", "queso"]
 
-app.use("/",express.static("../public", {index: "index.html"}))
+app.use("/",express.static("../frontend/build/", {index: "index.html"}))
 
 app.get("/random_food/",(req, res)=>{
     const randomIndex = Math.floor(Math.random()*(food.length-1))
