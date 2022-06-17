f674873728812ea9944851b7f29aa8cc10509b56
DanBaDo <byte4life@pm.me>
2022-06-17 12:16:27 +0200

    Now frontend gets dishes in DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,37 +19,53 @@ function App() {
       break;
   }
 
-  const [ food, setFood ] = useState("...")
+  const [ dishesList, setDishesList ] = useState([])
+  const [ dish, setDish ] = useState("...")
+  const [ url, setUrl ] = useState("")
   const [ showForm, setShowForm ] = useState(false)
 
-  async function fetchFood () {
-    const response = await fetch(HOST+"random_food/")
-    const data = await response.text()
-    setFood(data) 
+  async function fetchDishesList () {
+    const response = await fetch(HOST+"dishes/")
+    const data = await response.json()
+    setDishesList(data)
   }
 
-  async function floatingButtonClickHandler (event) {
-    console.log("...");
+  async function changeUrlHandler (event) {
+    if (event.target.validity.valid) setUrl(event.target.value)
+  }
+
+  async function floatingButtonClickHandler () {
     setShowForm(!showForm)
   }
 
   async function submitHandler (event) {
-    console.log("...");
+    event.preventDefault()
+    setShowForm(false)
+    setUrl('')
+    console.log(event);
   }
 
   useEffect(
     ()=>{
-      fetchFood()
+      fetchDishesList()
     },
     []
   )
 
+  useEffect(
+    ()=>{
+      const randomIndex = Math.floor(Math.random()*(dishesList.length-1))
+      setDish(dishesList[randomIndex]?.dish)
+    },
+    [dishesList]
+  )
+
   return (
     <div className="wrapper">
-      <h1>Hoy toca comer {food}.</h1>
+      <h1>Hoy toca comer {dish}.</h1>
 
       <form className={showForm ? "show form" : "form"} onSubmit={submitHandler}>
-        <input type={"url"}/>
+        <input type={"url"} name="url" placeholder="write yout recipe URL" value={url} onChange={changeUrlHandler}/>
         <input type={"submit"}/>
       </form>
       
