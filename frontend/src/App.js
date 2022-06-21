a395e040801c50a5b4d7e383fcf3f70a973ae92e
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-06-21 07:17:42 +0200

    S3 notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -107,3 +107,44 @@ function App() {
 }
 
 export default App;
+
+/*
+const s3 = new aws.S3({
+  apiVersion: process.env.S3_API_VERSION,
+  endpoint: process.env.S3_ENDPOINT,
+  signatureVersion: process.env.S3_SIGNATURE_VERSION,
+})
+*/
+
+/*
+app.post("/upload/:articleId", async (req, res)=>{
+    try {
+
+        const s3ObjectKey = Date.now().toString()
+
+        const s3Response = s3.upload({ //s3.putObject()
+            Bucket: process.env.S3_BUCKET,
+            Key: s3ObjectKey,
+            Body: req,
+            ContentType: req.headers['content-type'],
+            ContentLength: req.headers['content-length']
+        })
+
+        const data = await s3Response.promise()
+
+        const articleIdx = mockedDB.findIndex(
+            item => item.id === req.params.articleId
+        )
+
+        mockedDB[articleIdx].imageKey = data.Key
+        mockedDB[articleIdx].imagePublicURL = data.Location
+
+        res.sendStatus(201)
+        
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
+
+})
+*/
